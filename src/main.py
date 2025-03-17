a907ee1cd2dcaf3bd4f4d3ab82b9b35f0619c539
Gussy97 <angus.scheiwe1997@hotmail.com>
2025-03-17 16:34:33 +1000

    added copy files

--- src/main.py
+++ src/main.py
@@ -1,32 +1,20 @@
-from inline_markdown import *
-from block_markdown import *
+import os
+import shutil
+from copystatic import copy_files_recursive
 
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-   markdown = """
-# This is a heading
+   print("Deleting Public Folder")
+   delete_public()
+   print("Copying files from static to public")
+   copy_files_recursive(dir_path_static, dir_path_public)
 
-This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
 
-- This is the first list item in a list block
-- This is a list item
-- This is another list item
+def delete_public():
+   if os.path.exists(dir_path_public):
+      shutil.rmtree(dir_path_public)
 
-```
-This is code
-```
 
->This is a quote
->That continues **to** this line
->and this _line_
-
-1. this is the first _item_ in an ordered list
-2. and this is the `second`
-3. and this is the **third**
-
-"""
-   for block in markdown_to_blocks(markdown):
-      if block_to_blocktype(block) in (BlockType.ORDERED_LIST,):
-         for node in get_nodes_from_blocks([block]):
-            print(node.to_html())
 main()
